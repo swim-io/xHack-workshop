8535771a18bcba74282e2919fa32e733a0756337
Nico Miicro <nico@swim.io>
2022-10-06 11:11:12 +0300

    refactor(SwapForm): use ?. for existence check

--- browser/utils.ts
+++ browser/utils.ts
@@ -24,5 +24,5 @@ export const handleEvent =
       tx: event.transactionHash,
       block: event.blockHash,
     });
-    callback && callback({ txId: event.transactionHash, chain: chainId });
+    callback?.({ txId: event.transactionHash, chain: chainId });
   };
