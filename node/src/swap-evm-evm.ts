5e12143631fa1b2a9c01d4b6a20d20ac820d6694
wormat <wormat@maelstrom.finance>
2022-10-07 19:18:57 +0200

    refactor(node): Use utils in swap scripts and tidy

--- node/src/swap-evm-evm.ts
+++ node/src/swap-evm-evm.ts
@@ -1,62 +1,27 @@
-import crypto from "crypto";
-
 import { CHAINS, parseSequenceFromLogEth } from "@certusone/wormhole-sdk";
-import type { ChainConfig } from "@swim-io/core";
-import { Env, getTokenDetails } from "@swim-io/core";
-import { avalanche, bnb, ethereum, polygon } from "@swim-io/evm";
+import { getTokenDetails } from "@swim-io/core";
+import { evmAddressToWormhole } from "@swim-io/evm";
 import { ERC20Token__factory, Routing__factory } from "@swim-io/evm-contracts";
 import { TOKEN_PROJECTS_BY_ID, TokenProjectId } from "@swim-io/token-projects";
-import type { Event, Overrides } from "ethers";
-import { Wallet, providers, utils } from "ethers";
+import type { Overrides } from "ethers";
+import { Wallet as EvmWallet, utils } from "ethers";
 
-type SupportedChains = "avalanche" | "bsc" | "ethereum" | "polygon";
-type Chain = typeof CHAINS[SupportedChains];
-
-const RPC_URLS: Record<Chain, string | undefined> = {
-  [CHAINS.avalanche]: process.env.AVALANCHE_RPC,
-  [CHAINS.bsc]: process.env.BNB_RPC,
-  [CHAINS.ethereum]: process.env.ETHEREUM_RPC,
-  [CHAINS.polygon]: process.env.POLYGON_RPC,
-};
-
-const CHAIN_CONFIGS: Record<Chain, ChainConfig> = {
-  [CHAINS.avalanche]: avalanche.chains[Env.Testnet],
-  [CHAINS.bsc]: bnb.chains[Env.Testnet],
-  [CHAINS.ethereum]: ethereum.chains[Env.Testnet],
-  [CHAINS.polygon]: polygon.chains[Env.Testnet],
-};
-
-const EVM_BYTES_LOG_LENGTH = 32;
-const SWIM_MEMO_LENGTH = 16;
-const WORMHOLE_ADDRESS_LENGTH = 32;
-
-const bufferToBytesFilter = (buffer: Buffer): Buffer =>
-  Buffer.concat([buffer, Buffer.alloc(EVM_BYTES_LOG_LENGTH - buffer.length)]);
-
-const logEvent = (chain: "source" | "target", log: string, event: Event) => {
-  console.table({
-    label: `Propeller tx detected on ${chain} chain`,
-    memo: log.replace(/^0x/, ""),
-    tx: event.transactionHash,
-    block: event.blockHash,
-  });
-};
-
-const createProvider = (chain: Chain): providers.JsonRpcProvider => {
-  const rpc = RPC_URLS[chain];
-  if (!rpc) {
-    console.error(`Missing RPC env variable for chain ${chain}`);
-    process.exit(1);
-  }
-  return new providers.JsonRpcProvider(rpc);
-};
+import type { EvmChain } from "./config";
+import { EVM_CHAIN_CONFIGS } from "./config";
+import {
+  bufferToEvmBytesFilter,
+  createEvmKeypair,
+  createEvmProvider,
+  logEvmEvent,
+} from "./evmUtils";
+import { createMemo } from "./swimUtils";
 
 interface SwapParameters {
   readonly mnemonic: string;
   readonly hdPath: string;
-  readonly sourceChain: Chain;
+  readonly sourceChain: EvmChain;
   readonly sourceTokenProjectId: TokenProjectId;
-  readonly targetChain: Chain;
+  readonly targetChain: EvmChain;
   readonly targetTokenProjectId: TokenProjectId;
   /** In human units */
   readonly inputAmount: string;
@@ -79,28 +44,44 @@ const swap = async ({
   gasKickStart = false,
   overrides = {},
 }: SwapParameters): Promise<void> => {
+  /**
+   * STEP 1: Get chain configs and find token projects
+   */
   if (sourceChain === targetChain) {
     throw new Error("Invalid target chain");
   }
-  const sourceChainConfig = CHAIN_CONFIGS[sourceChain];
-  const targetChainConfig = CHAIN_CONFIGS[targetChain];
+  const sourceChainConfig = EVM_CHAIN_CONFIGS[sourceChain];
+  const targetChainConfig = EVM_CHAIN_CONFIGS[targetChain];
   const sourceTokenProject = TOKEN_PROJECTS_BY_ID[sourceTokenProjectId];
+  if (
+    sourceTokenProjectId !== TokenProjectId.SwimUsd &&
+    sourceTokenProject.tokenNumber === null
+  ) {
+    throw new Error("Invalid source token");
+  }
   const targetTokenProject = TOKEN_PROJECTS_BY_ID[targetTokenProjectId];
-  console.info("=".repeat(process.stdout.columns));
-  console.info(
-    `* ${sourceChainConfig.name} ${sourceTokenProject.symbol} -> ${targetChainConfig.name} ${targetTokenProject.symbol}`,
-  );
   if (targetTokenProject.tokenNumber === null) {
     throw new Error("Invalid target token");
   }
 
-  const account = utils.HDNode.fromMnemonic(mnemonic).derivePath(hdPath);
-  console.info(`Account address: ${account.address}`);
+  console.info("=".repeat(process.stdout.columns));
+  console.info(
+    `* ${sourceChainConfig.name} ${sourceTokenProject.symbol} -> ${targetChainConfig.name} ${targetTokenProject.symbol}`,
+  );
 
-  const sourceProvider = createProvider(sourceChain);
-  const targetProvider = createProvider(targetChain);
-  const sourceWallet = new Wallet(account, sourceProvider);
+  /**
+   * STEP 2: Set up wallet and providers
+   */
+  const keypair = createEvmKeypair(mnemonic, hdPath);
+  console.info(`Account address: ${keypair.address}`);
 
+  const sourceProvider = createEvmProvider(sourceChain);
+  const targetProvider = createEvmProvider(targetChain);
+  const sourceWallet = new EvmWallet(keypair, sourceProvider);
+
+  /**
+   * STEP 3: Connect to smart contracts
+   */
   const sourceRoutingContract = Routing__factory.connect(
     sourceChainConfig.routingContractAddress,
     sourceWallet,
@@ -127,12 +108,15 @@ const swap = async ({
     targetProvider,
   );
 
+  /**
+   * STEP 4: Fetch and display initial balances
+   */
   const getBalances = async () => {
     const [sourceGas, targetGas, sourceToken, targetToken] = await Promise.all([
-      sourceProvider.getBalance(account.address),
-      targetProvider.getBalance(account.address),
-      sourceTokenContract.balanceOf(account.address),
-      targetTokenContract.balanceOf(account.address),
+      sourceProvider.getBalance(keypair.address),
+      targetProvider.getBalance(keypair.address),
+      sourceTokenContract.balanceOf(keypair.address),
+      targetTokenContract.balanceOf(keypair.address),
     ]);
     return {
       sourceGas: utils.formatEther(sourceGas),
@@ -147,6 +131,9 @@ const swap = async ({
     ...initialBalances,
   });
 
+  /**
+   * STEP 5: Approve ERC20 token spend if required
+   */
   const inputAmountAtomic = utils.parseUnits(
     inputAmount,
     sourceTokenDetails.decimals,
@@ -166,28 +153,29 @@ const swap = async ({
     await approvalResponse.wait();
   }
 
-  const targetOwner = utils.hexZeroPad(
-    account.address,
-    WORMHOLE_ADDRESS_LENGTH,
-  );
-
+  /**
+   * STEP 6: Gather arguments for propeller transfer
+   */
+  const targetOwner = evmAddressToWormhole(keypair.address);
   const maxPropellerFeeAtomic = utils.parseUnits(
     maxPropellerFee,
     targetChainConfig.swimUsdDetails.decimals,
   );
-  // NOTE: Please always use random bytes to avoid conflicts with other users
-  const memo = crypto.randomBytes(SWIM_MEMO_LENGTH);
+  const memo = createMemo();
 
+  /**
+   * STEP 7: Subscribe to events on source and target chains
+   */
   const sourceFilter = sourceRoutingContract.filters.MemoInteraction(
-    bufferToBytesFilter(memo),
+    bufferToEvmBytesFilter(memo),
   );
-  sourceRoutingContract.once(sourceFilter, logEvent.bind(null, "source"));
+  sourceRoutingContract.once(sourceFilter, logEvmEvent.bind(null, "source"));
   const targetFilter = targetRoutingContract.filters.MemoInteraction(
-    bufferToBytesFilter(memo),
+    bufferToEvmBytesFilter(memo),
   );
   const promiseToReturn = new Promise<void>((resolve, reject) => {
     targetRoutingContract.once(targetFilter, (log, event) => {
-      logEvent("target", log, event);
+      logEvmEvent("target", log, event);
       getBalances()
         .then((finalBalances) => {
           console.table({
@@ -199,18 +187,21 @@ const swap = async ({
     });
   });
 
+  /**
+   * STEP 8: Initiate propeller interaction
+   */
   console.table({
-    label: "Propeller kick-off tx params",
+    label: "Initiate propeller tx params",
     sourceToken: sourceTokenDetails.address,
     inputAmountAtomic: inputAmountAtomic.toString(),
     targetChain,
-    targetOwner,
+    targetOwner: Buffer.from(targetOwner).toString("hex"),
     gasKickStart,
     maxPropellerFee: maxPropellerFeeAtomic.toString(),
     targetTokenNumber: targetTokenProject.tokenNumber,
     memo: memo.toString("hex"),
   });
-  const kickOffResponse = await sourceRoutingContract[
+  const initiatePropellerTxResponse = await sourceRoutingContract[
     "propellerInitiate(address,uint256,uint16,bytes32,bool,uint64,uint16,bytes16)"
   ](
     sourceTokenDetails.address,
@@ -224,29 +215,46 @@ const swap = async ({
     overrides,
   );
   console.info(
-    `Source chain kick-off transaction hash: ${kickOffResponse.hash}`,
+    `Source chain initiate propeller transaction hash: ${initiatePropellerTxResponse.hash}`,
   );
 
-  const kickOffReceipt = await kickOffResponse.wait();
+  /**
+   * STEP 9: Display Wormhole sequence number for debugging
+   */
+  const initiatePropellerTxReceipt = await initiatePropellerTxResponse.wait();
   const sequence = parseSequenceFromLogEth(
-    kickOffReceipt,
+    initiatePropellerTxReceipt,
     sourceChainConfig.wormhole.bridge,
   );
   console.info(`Wormhole sequence: ${sequence}`);
 
+  /**
+   * STEP 10: Wait for transactions to appear and log final balances
+   */
   return promiseToReturn;
 };
 
 const main = async (): Promise<void> => {
   const { EVM_HD_PATH, EVM_MNEMONIC } = process.env;
-  if (!EVM_HD_PATH) {
-    console.error("Please set EVM_HD_PATH");
-    process.exit(1);
-  }
   if (!EVM_MNEMONIC) {
     console.error("Please set EVM_MNEMONIC");
     process.exit(1);
   }
+  if (!EVM_HD_PATH) {
+    console.error("Please set EVM_HD_PATH");
+    process.exit(1);
+  }
+
+  await swap({
+    mnemonic: EVM_MNEMONIC,
+    hdPath: EVM_HD_PATH,
+    sourceChain: CHAINS.bsc,
+    sourceTokenProjectId: TokenProjectId.SwimUsd,
+    targetChain: CHAINS.ethereum,
+    targetTokenProjectId: TokenProjectId.Usdc,
+    inputAmount: "6.23",
+    maxPropellerFee: "5.1",
+  });
 
   await swap({
     mnemonic: EVM_MNEMONIC,
@@ -297,4 +305,7 @@ const main = async (): Promise<void> => {
   });
 };
 
-main().catch(console.error);
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
--- node/src/swap-evm-solana.ts
+++ node/src/swap-evm-solana.ts
@@ -1,94 +1,28 @@
-import crypto from "crypto";
-
 import { CHAINS, parseSequenceFromLogEth } from "@certusone/wormhole-sdk";
-import {
-  Program,
-  AnchorProvider,
-  Wallet as AnchorWallet,
-} from "@project-serum/anchor";
 import { getAssociatedTokenAddress } from "@solana/spl-token";
-import { Connection, Keypair, PublicKey } from "@solana/web3.js";
-import type { ChainConfig } from "@swim-io/core";
-import { Env, getTokenDetails } from "@swim-io/core";
-import { avalanche, bnb, ethereum, fantom, polygon } from "@swim-io/evm";
+import { PublicKey } from "@solana/web3.js";
+import { getTokenDetails } from "@swim-io/core";
 import { ERC20Token__factory, Routing__factory } from "@swim-io/evm-contracts";
 import { solana } from "@swim-io/solana";
-import { idl } from "@swim-io/solana-contracts";
 import { TOKEN_PROJECTS_BY_ID, TokenProjectId } from "@swim-io/token-projects";
-import * as bip39 from "bip39";
-import { derivePath } from "ed25519-hd-key";
-import type { Event, Overrides } from "ethers";
-import { Wallet, providers, utils } from "ethers";
+import type { Overrides } from "ethers";
+import { utils } from "ethers";
 
-type SupportedEvmChains =
-  | "avalanche"
-  | "bsc"
-  | "ethereum"
-  | "fantom"
-  | "polygon";
-type EvmChain = typeof CHAINS[SupportedEvmChains];
-type SolanaChain = typeof CHAINS.solana;
-
-const EVM_RPC_URLS: Record<EvmChain, string | undefined> = {
-  [CHAINS.avalanche]: process.env.AVALANCHE_RPC,
-  [CHAINS.bsc]: process.env.BNB_RPC,
-  [CHAINS.ethereum]: process.env.ETHEREUM_RPC,
-  [CHAINS.fantom]: process.env.FANTOM_RPC,
-  [CHAINS.polygon]: process.env.POLYGON_RPC,
-};
-
-const SOLANA_RPC_URL = process.env.SOLANA_RPC;
-
-const EVM_CHAIN_CONFIGS: Record<EvmChain, ChainConfig> = {
-  [CHAINS.avalanche]: avalanche.chains[Env.Testnet],
-  [CHAINS.bsc]: bnb.chains[Env.Testnet],
-  [CHAINS.ethereum]: ethereum.chains[Env.Testnet],
-  [CHAINS.fantom]: fantom.chains[Env.Testnet],
-  [CHAINS.polygon]: polygon.chains[Env.Testnet],
-};
-
-const SOLANA_CHAIN_CONFIG = solana.chains[Env.Testnet];
-
-const EVM_BYTES_LOG_LENGTH = 32;
-const SWIM_MEMO_LENGTH = 16;
-
-const bufferToEvmBytesFilter = (buffer: Buffer): Buffer =>
-  Buffer.concat([buffer, Buffer.alloc(EVM_BYTES_LOG_LENGTH - buffer.length)]);
-
-const logEvmEvent = (log: string, event: Event) => {
-  console.table({
-    label: "Propeller tx detected on EVM chain",
-    memo: log.replace(/^0x/, ""),
-    tx: event.transactionHash,
-    block: event.blockHash,
-  });
-};
-
-const createEvmProvider = (chain: EvmChain): providers.JsonRpcProvider => {
-  const rpc = EVM_RPC_URLS[chain];
-  if (!rpc) {
-    console.error(`Missing RPC env variable for chain ${chain}`);
-    process.exit(1);
-  }
-  return new providers.JsonRpcProvider(rpc);
-};
-
-const createSolanaProvider = (wallet: AnchorWallet): AnchorProvider => {
-  if (!SOLANA_RPC_URL) {
-    console.error("Missing RPC env variable for Solana");
-    process.exit(1);
-  }
-  const connection = new Connection(SOLANA_RPC_URL);
-  return new AnchorProvider(
-    connection,
-    {
-      publicKey: wallet.publicKey,
-      signAllTransactions: wallet.signAllTransactions,
-      signTransaction: wallet.signTransaction,
-    },
-    {},
-  );
-};
+import type { EvmChain } from "./config";
+import { EVM_CHAIN_CONFIGS, SOLANA_CHAIN_CONFIG } from "./config";
+import {
+  bufferToEvmBytesFilter,
+  createEvmProvider,
+  createEvmWallet,
+  logEvmEvent,
+} from "./evmUtils";
+import {
+  createSolanaConnection,
+  createSolanaKeypair,
+  getOrCreateSolanaTokenAccounts,
+  logSolanaAccounts,
+} from "./solanaUtils";
+import { createMemo } from "./swimUtils";
 
 interface SwapParameters {
   readonly evmMnemonic: string;
@@ -97,7 +31,6 @@ interface SwapParameters {
   readonly solanaHdPath: string;
   readonly sourceChain: EvmChain;
   readonly sourceTokenProjectId: TokenProjectId;
-  readonly targetChain: SolanaChain;
   readonly targetTokenProjectId: TokenProjectId;
   /** In human units */
   readonly inputAmount: string;
@@ -115,46 +48,51 @@ const swap = async ({
   solanaHdPath,
   sourceChain,
   sourceTokenProjectId,
-  targetChain,
   targetTokenProjectId,
   inputAmount,
   maxPropellerFee,
   gasKickStart = false,
   overrides = {},
 }: SwapParameters): Promise<void> => {
+  /**
+   * STEP 1: Get EVM chain config and find token projects
+   */
   const evmChainConfig = EVM_CHAIN_CONFIGS[sourceChain];
-  const solanaChainConfig = SOLANA_CHAIN_CONFIG;
   const evmTokenProject = TOKEN_PROJECTS_BY_ID[sourceTokenProjectId];
+  if (
+    // NOTE: SDK will be updated to handle this case
+    sourceTokenProjectId !== TokenProjectId.SwimUsd &&
+    evmTokenProject.tokenNumber === null
+  ) {
+    throw new Error("Invalid source token");
+  }
   const solanaTokenProject = TOKEN_PROJECTS_BY_ID[targetTokenProjectId];
-  console.info("=".repeat(process.stdout.columns));
-  console.info(
-    `* ${evmChainConfig.name} ${evmTokenProject.symbol} -> ${solanaChainConfig.name} ${solanaTokenProject.symbol}`,
-  );
   if (solanaTokenProject.tokenNumber === null) {
     throw new Error("Invalid target token");
   }
 
-  const evmAccount =
-    utils.HDNode.fromMnemonic(evmMnemonic).derivePath(evmHdPath);
-  console.info(`EVM account address: ${evmAccount.address}`);
-  const evmProvider = createEvmProvider(sourceChain);
-  const evmWallet = new Wallet(evmAccount, evmProvider);
-
-  const solanaSeed = await bip39.mnemonicToSeed(solanaMnemonic, "");
-  const solanaKeypair = Keypair.fromSeed(
-    derivePath(solanaHdPath, solanaSeed.toString("hex")).key,
+  console.info("=".repeat(process.stdout.columns));
+  console.info(
+    `* ${evmChainConfig.name} ${evmTokenProject.symbol} -> ${SOLANA_CHAIN_CONFIG.name} ${solanaTokenProject.symbol}`,
   );
-  const solanaWallet = new AnchorWallet(solanaKeypair);
-  console.info("Solana account address", solanaWallet.publicKey.toBase58());
-  const solanaProvider = createSolanaProvider(solanaWallet);
 
+  /**
+   * STEP 2: Set up wallets and providers
+   */
+  const evmProvider = createEvmProvider(sourceChain);
+  const evmWallet = createEvmWallet(evmMnemonic, evmHdPath, evmProvider);
+  console.info(`EVM account address: ${evmWallet.address}`);
+
+  const solanaKeypair = await createSolanaKeypair(solanaMnemonic, solanaHdPath);
+  console.info("Solana account address", solanaKeypair.publicKey.toBase58());
+  const solanaConnection = createSolanaConnection();
+
+  /**
+   * STEP 3: Connect to EVM smart contracts on source chain
+   */
   const evmRoutingContract = Routing__factory.connect(
     evmChainConfig.routingContractAddress,
     evmWallet,
-  );
-  const solanaRoutingContract = new Program(
-    idl.propeller,
-    solanaChainConfig.routingContractAddress,
   );
 
   const evmTokenDetails = getTokenDetails(evmChainConfig, sourceTokenProjectId);
@@ -162,22 +100,35 @@ const swap = async ({
     evmTokenDetails.address,
     evmWallet,
   );
+
+  /**
+   * STEP 4: Create SPL token accounts if required
+   */
+  const userTokenAccounts = await getOrCreateSolanaTokenAccounts(
+    solanaConnection,
+    solanaKeypair,
+  );
+  logSolanaAccounts("User SPL token accounts", userTokenAccounts);
+
+  /**
+   * STEP 5: Fetch and display initial balances
+   */
   const solanaTokenDetails = getTokenDetails(
-    solanaChainConfig,
+    SOLANA_CHAIN_CONFIG,
     targetTokenProjectId,
   );
   const solanaTokenAccount = await getAssociatedTokenAddress(
     new PublicKey(solanaTokenDetails.address),
-    solanaWallet.publicKey,
+    solanaKeypair.publicKey,
   );
 
   const getBalances = async () => {
     const [evmGas, solanaGas, evmToken, solanaTokenResponse] =
       await Promise.all([
-        evmProvider.getBalance(evmAccount.address),
-        solanaProvider.connection.getBalance(solanaWallet.publicKey),
-        evmTokenContract.balanceOf(evmAccount.address),
-        solanaProvider.connection.getTokenAccountBalance(solanaTokenAccount),
+        evmProvider.getBalance(evmWallet.address),
+        solanaConnection.getBalance(solanaKeypair.publicKey),
+        evmTokenContract.balanceOf(evmWallet.address),
+        solanaConnection.getTokenAccountBalance(solanaTokenAccount),
       ]);
     return {
       evmGas: utils.formatEther(evmGas),
@@ -195,6 +146,9 @@ const swap = async ({
     ...initialBalances,
   });
 
+  /**
+   * STEP 6: Approve ERC20 token spend if required
+   */
   const inputAmountAtomic = utils.parseUnits(
     inputAmount,
     evmTokenDetails.decimals,
@@ -214,29 +168,37 @@ const swap = async ({
     await approvalResponse.wait();
   }
 
+  /**
+   * STEP 7: Gather arguments for propeller transfer
+   */
   const solanaOwner = solanaTokenAccount.toBytes();
-
   const maxPropellerFeeAtomic = utils.parseUnits(
     maxPropellerFee,
-    solanaChainConfig.swimUsdDetails.decimals,
+    SOLANA_CHAIN_CONFIG.swimUsdDetails.decimals,
   );
-  // NOTE: Please always use random bytes to avoid conflicts with other users
-  const memo = crypto.randomBytes(SWIM_MEMO_LENGTH);
+  const memo = createMemo();
 
+  /**
+   * STEP 8: Subscribe to events on source and target chains
+   */
   const evmFilter = evmRoutingContract.filters.MemoInteraction(
     bufferToEvmBytesFilter(memo),
   );
-  evmRoutingContract.once(evmFilter, logEvmEvent);
+  evmRoutingContract.once(evmFilter, logEvmEvent.bind(null, "source"));
   const promiseToReturn = new Promise<void>((resolve, reject) => {
-    solanaProvider.connection.onLogs(
-      solanaRoutingContract.programId,
+    solanaConnection.onLogs(
+      new PublicKey(SOLANA_CHAIN_CONFIG.routingContractAddress),
       (logs, context) => {
-        console.log("FILTERED ON SOLANA", logs, context);
-        // TODO: Fill out
         const didFindMemo = logs.logs.find(
           (log) => log.indexOf(memo.toString("hex")) !== -1,
         );
         if (didFindMemo) {
+          console.table({
+            label: "Propeller tx detected on target chain",
+            memo,
+            tx: logs.signature,
+            block: context.slot,
+          });
           getBalances()
             .then((finalBalances) => {
               console.table({
@@ -250,23 +212,26 @@ const swap = async ({
     );
   });
 
+  /**
+   * STEP 9: Initiate propeller interaction
+   */
   console.table({
-    label: "Propeller kick-off tx params",
+    label: "Initiate propeller tx params",
     evmToken: evmTokenDetails.address,
     inputAmountAtomic: inputAmountAtomic.toString(),
-    targetChain,
+    targetChain: CHAINS.solana,
     solanaOwner: Buffer.from(solanaOwner).toString("hex"),
     gasKickStart,
     maxPropellerFee: maxPropellerFeeAtomic.toString(),
     targetTokenNumber: solanaTokenProject.tokenNumber,
     memo: memo.toString("hex"),
   });
-  const kickOffResponse = await evmRoutingContract[
+  const initiatePropellerTxResponse = await evmRoutingContract[
     "propellerInitiate(address,uint256,uint16,bytes32,bool,uint64,uint16,bytes16)"
   ](
     evmTokenDetails.address,
     inputAmountAtomic,
-    targetChain,
+    CHAINS.solana,
     solanaOwner,
     gasKickStart,
     maxPropellerFeeAtomic,
@@ -275,36 +240,42 @@ const swap = async ({
     overrides,
   );
   console.info(
-    `Source chain kick-off transaction hash: ${kickOffResponse.hash}`,
+    `Source chain initiate propeller transaction hash: ${initiatePropellerTxResponse.hash}`,
   );
 
-  const kickOffReceipt = await kickOffResponse.wait();
+  /**
+   * STEP 10: Display Wormhole sequence number for debugging
+   */
+  const initatePropellerTxReceipt = await initiatePropellerTxResponse.wait();
   const sequence = parseSequenceFromLogEth(
-    kickOffReceipt,
+    initatePropellerTxReceipt,
     evmChainConfig.wormhole.bridge,
   );
   console.info(`Wormhole sequence: ${sequence}`);
 
+  /**
+   * STEP 11: Wait for transactions to appear and log final balances
+   */
   return promiseToReturn;
 };
 
 const main = async (): Promise<void> => {
   const { EVM_HD_PATH, EVM_MNEMONIC, SOLANA_HD_PATH, SOLANA_MNEMONIC } =
     process.env;
-  if (!EVM_HD_PATH) {
-    console.error("Please set EVM_HD_PATH");
-    process.exit(1);
-  }
   if (!EVM_MNEMONIC) {
     console.error("Please set EVM_MNEMONIC");
     process.exit(1);
   }
-  if (!SOLANA_HD_PATH) {
-    console.error("Please set SOLANA_HD_PATH");
+  if (!EVM_HD_PATH) {
+    console.error("Please set EVM_HD_PATH");
     process.exit(1);
   }
   if (!SOLANA_MNEMONIC) {
     console.error("Please set SOLANA_MNEMONIC");
+    process.exit(1);
+  }
+  if (!SOLANA_HD_PATH) {
+    console.error("Please set SOLANA_HD_PATH");
     process.exit(1);
   }
 
@@ -314,12 +285,26 @@ const main = async (): Promise<void> => {
     solanaMnemonic: SOLANA_MNEMONIC,
     solanaHdPath: SOLANA_HD_PATH,
     sourceChain: CHAINS.bsc,
+    sourceTokenProjectId: TokenProjectId.SwimUsd,
+    targetTokenProjectId: TokenProjectId.Usdc,
+    inputAmount: "1.23",
+    maxPropellerFee: "5.1",
+  });
+
+  await swap({
+    evmMnemonic: EVM_MNEMONIC,
+    evmHdPath: EVM_HD_PATH,
+    solanaMnemonic: SOLANA_MNEMONIC,
+    solanaHdPath: SOLANA_HD_PATH,
+    sourceChain: CHAINS.bsc,
     sourceTokenProjectId: TokenProjectId.Usdt,
-    targetChain: CHAINS.solana,
     targetTokenProjectId: TokenProjectId.Usdc,
     inputAmount: "1.23",
     maxPropellerFee: "5.1",
   });
 };
 
-main().catch(console.error);
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
